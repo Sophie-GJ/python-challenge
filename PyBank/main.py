5fe35cf5c2f81e6c9006164b479687a31a035ce2
Sophie Geister-Jones <144928855+Sophie-GJ@users.noreply.github.com>
2023-10-21 16:50:06 -0500

    finished bulk of challenge
    still need to export to txt file but got all the variables and data to run

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,43 +11,42 @@ with open(budget_data) as monthly_budget:
 
 #Calculate total number of months in the budget_data.
     #YOU NEED TO make a for loop where total months starts at 0 and each row adds 1 to that value.
-    total_months as double = 0
-    #is this how we define a variable in python? my brain has too much VBA in it to be anything but troublesome.
-    for 
-    #sophie this is WRONG. 
-    print(f"The total months used in this data set is {total_months} months.")
+    total_months=0
+    total_profits_losses=0
+    total_monthly_difference=0
+    max_increase_monthly_change=0
+    max_decrease_monthly_change=0
+    increase_month=""
+    decrease_month=""
 
-#Calculate the net total of Profit/Losses over the entire period
-    #just add up the entire profits/losses column in the csv?
-    #do we need to define that column as long?
-    #do we do the same thing we did up above but instead of adding 1 add the value of the column?
-    net_total as int = 0
+    for rownum in csvreader:
+        #adding one to total months, reassigning it
+        total_months = total_months+1
 
-#Calculate changes in Profit/Losses over the entire period
-    #define monthly_change, nextrow, current row (as long variables?)
-    currentrow = 
-    nextrow =
-    monthly_change = nextrow-currentrow
+        total_profits_losses = total_profits_losses+int(rownum[1])
+       
+        #so it doesn't calculate difference for first number
+        if total_months>1:
+            total_monthly_difference=total_monthly_difference+int(rownum[1])-previous_month_value
+            
+            current_monthly_change=int(rownum[1])-previous_month_value
+            if current_monthly_change>max_increase_monthly_change:
+                max_increase_monthly_change=current_monthly_change
+                increase_month=rownum[0]
 
-    #make a loop to add up all the monthly_change values to = total_profit_loss_change
-    #do we need a previous_monthly_change variable? am i thinking too hard?
-    #do we make a third column in the csv file with this? to store it?
-    total_profit_loss_change as long = 
+            if current_monthly_change<max_decrease_monthly_change:
+                max_decrease_monthly_change=current_monthly_change
+                decrease_month=rownum[0]
+            
+        #keep the previous_month_value at the bottom because python reads top to bottom and it will not change before it is used to calculate total_monthly_differnece_
+        previous_month_value = int(rownum[1])
 
-
-#average those changes
-    #divide total_profit_loss_change by (total_months - 1) (because there is no profit/loss change for first value)
-        #do these need to be defined as the same type of variable?
-
-#find the greatest increase in profits (date and amount) over the entire period
-    #this is why i think we need to make a third column but I'm bonkers confused.
-
-#find the greatest decrease in profits (date and amount) over the entire period
 
 #Print the following to the terminal and export a text file with the following information.
+
 print("Financial Analysis")
-print(f"Total Months: " {total_months})
-print(f"Total Profits/Losses: " {net_total})
-print(f"Average Change: " {mean_monthly_change})
-print(f"Greatest Increase in Profits: " {max_increase_monthly_change})
-print(f"Total Profits/Losses: " {max_decrease_monthly_change})
+print(f"Total Months: {total_months}")
+print(f"Total Profits/Losses: {total_profits_losses}")
+print(f"Average Change: ${(round(total_monthly_difference/(total_months-1), 2))}")
+print(f"Greatest Increase in Profits: {increase_month}, ${max_increase_monthly_change}")
+print(f"Greatest Decrease in Profits: {decrease_month}, ${max_decrease_monthly_change}")
