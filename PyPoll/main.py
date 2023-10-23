58ffead2dca9cb0f71259784642bcbe367b3be26
Sophie Geister-Jones <144928855+Sophie-GJ@users.noreply.github.com>
2023-10-22 22:37:10 -0500

    election results found, no clean up
    I managed to get the bulk of the election results in some form or the other, now just struggling to wrangle what I have into easily readable analysis.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,64 +5,56 @@ import csv
 #Line 6 code courtesy of Tutor Kourt Bailey.
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+#create variables to hold information late on
+candidate_list=[]
+candidate_votes={}
+percent_list=[]
+
 #open and name the csv file and data within
 election_data=os.path.join('..', 'Resources', 'election_data.csv')
 with open(election_data) as ballot_county_candidate:
     csvreader=csv.reader(ballot_county_candidate, delimiter=',')
     csvheader=next(csvreader)
 
-#For Loop starting variable definitions
+    #create starting variable for total votes
     total_votes=0
-    stockham_votes=0
-    degette_votes=0
-    doane_votes=0
-    
-    for votes in csvreader:
+
+    #this loop finds the total number of votes, creates a list with candidate names, and counts the votes for the candidates
+    for rownum in csvreader:
+        #calculate total number of votes
         total_votes=total_votes+1
-        candidates=[votes[2]]
+        candidate_name=rownum[2]
+
+        #if candidate name isn't on candidate list, add it
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+
+            #create starting value
+            candidate_votes[candidate_name]=0
+
+        #count how many votes each candidate name on the list received
+        candidate_votes[candidate_name]=candidate_votes[candidate_name]+1
+
+    #this loop finds the candidate's percenage of votes
+    for candidate in candidate_votes:
+
+        #get specific candidate voter count alone
+        #code courtesy of AskBCS Learning Assistant
+        votes=candidate_votes.get(candidate)
+
+        #find percentage
+        candidate_percentage=float(votes)/float(total_votes)*100
         
-        if candidates=candidates+1:
-
+        #round percentage to 3 decimal places
+        rounded_percentage=round(candidate_percentage, 3)
         
-        
+        #add percentages to a list
+        percent_list.append(rounded_percentage)
 
+#Calculate the winner of the election
 
-
-
-
-            
-     
-
-
-
-# #Create a list of candidates
-#         if ballot==1:
-#             print(previousballet)
-#         elif previousballot!=ballot:
-#             print(ballot[3])
-    
-# #Total number of votes each candidate won
-# #if ballot[3] matches their name, count
-
-# #creating variable that allows us to search for different candidate names on the ballot
-# previousballot==ballot[3]
-
-#Percentage of votes each candidate won
-    #calculate individual candidate votes, create variables for each of them 
-    #keep to 3 decimals?
-#     stockham_percent=stockham_votes/total_votes
-#     degette_percent=degette_votes/total_votes
-#     doane_percent=doane_votes/total_votes
-#     #create a conditional if surname_votes_total = 0 because the math will get wonky?
-
-
-
-# #The winner of the election based on popular vote
-#     #find the individual_candiate_name with greatest_individual_candidate_vote
-#     winning_candidate=
-
-# #Print results in terminal and export a text file with the results)
+#Print results in terminal
 print("Election Results")
 print(f"Total Votes: {total_votes}")
-# print(f"{Individual_candidate_name}": {surname_vote_percent}" ({surname_total_votes})")
-# print(f"Winner: {winning_candidate}")
+
+#Export a file with results
